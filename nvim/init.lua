8eb8454d423d9584a4b9bc52f92d197ffeddc22c
Jens Olsson <jenson@axis.com>
2025-09-22 17:19:12 +0200

    Add extra-line errors

--- nvim/init.lua
+++ nvim/init.lua
@@ -174,3 +174,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
